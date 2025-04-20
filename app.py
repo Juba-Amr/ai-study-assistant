d08ddc526ab046e84e0bfcfe0b59c5fcb768c9f5
Juba-Amr <amari.juba2006@gmail.com>
2025-04-20 10:40:39 +0100

    implemented pdf upload and text extraction

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+
+number_of_pages = 0
+text_to_sumarize = ""
+st.title("Welcome to your AI assisstant")
+
+with st.form(key="get_pdf"):
+    st.header("Upload your file here")
+    uploaded_file = st.file_uploader(label='upload pdf', type='pdf')
+
+    st.form_submit_button("done")
+
+if uploaded_file != None :
+    with st.form(key="select_pages"):
+        reader = PdfReader(uploaded_file)
+        max_pages = reader._get_num_pages()
+        number_of_pages = st.slider(label='select how many pages you want to summarize',min_value=0, max_value=max_pages, step=1)
+
+        st.form_submit_button("done")
+
+for i in range(number_of_pages):
+    page = reader.pages[i]
+    text_to_sumarize += page.extract_text()
+
+print(text_to_sumarize)
